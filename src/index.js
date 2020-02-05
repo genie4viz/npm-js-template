35407a8c39a7cdca9b71ed95b6e71fc6e9deb199
selisteam <selistym@protonmail.com>
2020-02-04 20:33:43 -0800

    init for export to any project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from "lodash";
+import "./styles.css";
+import printMe from "./print.js";
+
+function component() {
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  btn.innerHTML = 'me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+  return element;
+}
+
+document.body.appendChild(component());
